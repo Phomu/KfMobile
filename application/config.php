91bc6e227fe335d68d26fd133d74138f9f267654
miaolapd <miaolapd@163.com>
2016-09-28 14:15:03 +0800

    ThinkPHP更新至5.0.1

--- application/config.php
+++ application/config.php
@@ -72,8 +72,6 @@ return [
     'auto_bind_module' => false,
     // 注册的根命名空间
     'root_namespace' => [],
-    // 扩展配置文件
-    'extra_config_list' => [],
     // 扩展函数文件
     'extra_file_list' => [THINK_PATH . 'helper' . EXT],
     // 默认输出类型
